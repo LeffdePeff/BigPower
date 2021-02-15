a0de93fbe99a22af5dc4b11bafd6e1196e4e1c1f
LeffdePeff <77458040+LeffdePeff@users.noreply.github.com>
2021-02-15 15:31:30 +0100

    Code Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-use gmp;
+use gmp::mpz::Mpz;
 
 use std::fs;
 use std::io::stdin;
 
 fn main()
 {   
-    let num1 = gmp::mpz::Mpz::from(get_input("Enter first number: ").trim().parse::<i32>().unwrap());
-    let num2 = get_input("Enter second number: ").trim().parse::<u32>().unwrap();
-    let length_bool:bool = get_input("Calculate length? true/false").trim().parse().unwrap();
-    let file_name = num1.to_string() + "^" + num2.to_string().as_str() + "_answer.txt";
-    let file_name_length = num1.to_string() + "^" + num2.to_string().as_str() + "_length.txt";
+    let first_number = Mpz::from(get_input("Enter first number: ").trim().parse::<i32>().unwrap());
+    let second_number = get_input("Enter second number: ").trim().parse::<u32>().unwrap();
+    let calculate_length_bool:bool = get_input("Calculate length? true/false").trim().parse().unwrap();
+    let file_name = first_number.to_string() + "^" + second_number.to_string().as_str() + "_answer.txt";
+    let file_name_length = first_number.to_string() + "^" + second_number.to_string().as_str() + "_length.txt";
 
     println!("Calculating...");
-    let answer = num1.pow(num2);
+    let answer = first_number.pow(second_number);
 
     println!("Converting answer to string...");
-    let str_answer = answer.to_string();
+    let answer_string = answer.to_string();
 
-    if length_bool {
+    if calculate_length_bool {
         println!("Calculating length...");
-        let answer_len = str_answer.len();
+        let answer_length = answer_string.len();
 
-        println!("The answer has {} numbers.", answer_len);
+        println!("The answer has {} numbers.", answer_length);
         
-        if answer_len <= 20 {
-            println!("The answer is: {}", str_answer);
+        if answer_length <= 20 {
+            println!("The answer is: {}", answer_length);
         }
 
         println!("Writing length to {} ...", file_name_length);
-        write_to_file(file_name_length, answer_len.to_string());
+        write_to_file(file_name_length, answer_length.to_string());
     }
 
     println!("Writing answer to {} ...", file_name);
-    write_to_file(file_name, str_answer);
+    write_to_file(file_name, answer_string);
 
     println!("Done!");
 }
